555f8fb1f050bc137bdf27fe6fbecb9f30f3e4f8
Khue-Vo <vo005642@stthomas.edu>
2026-04-16 03:42:07 -0500

    navigations

--- scripts/main.js
+++ scripts/main.js
@@ -32,6 +32,72 @@ let winCount = 0;
 let lossCount = 0;
 let delayDealerReveal = false;
 
+//Join Game button logic
+async function joinGame () {
+    try {
+        const response = await fetch('./scripts/config.json');
+        const data = await response.json();
+    
+        const nameInput = document.getElementById('nameInput').value.trim();
+        let finalUsername = nameInput || data.initialState.defaultUsername;
+
+        if (finalUsername) {
+            localStorage.setItem('username', finalUsername);
+        }
+
+        window.location.href = "player.html";
+    } catch (error) {
+        console.error('Error saving username to localStorage:', error);
+    }
+}
+const joinGameBtn = document.getElementById('join');
+joinGameBtn.addEventListener('click', joinGame);
+// Allow pressing Enter to join game
+window.addEventListener('keydown', event => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        joinGame();
+    }
+});
+// Pressing Esc to return to home page
+window.addEventListener("keydown", (event) => {
+    if (event.key === "Esc" || event.key === "Escape") {
+        event.preventDefault();
+        window.location.href = "index.html";
+    }
+});
+// Pressing I for instructions
+window.addEventListener('keydown', event => {
+    if (event.key === 'I' || event.key === 'i') {
+        event.preventDefault();
+        window.location.href = "instruction.html";
+    }
+});
+//Pressing L for leaderboard
+window.addEventListener('keydown', event => {
+    if (event.key === 'L' || event.key === 'l') {
+        event.preventDefault();
+        window.location.href = "leaderboard.html";
+    }
+});
+// Home click button 
+homeBtn.addEventListener("click", () => {
+    window.location.href = "index.html";
+});
+// Instructions click button 
+const instructionBtn = document.getElementById('instruction');
+instructionBtn.addEventListener('click', () => {
+    window.location.href = "instruction.html";
+});
+// Leaderboard click button 
+const leaderboardBtn = document.getElementById('leaderboard');
+leaderboardBtn.addEventListener('click', () => {
+    window.location.href = "leaderboard.html";
+});
+
+
+
+
 // Helper function to create an image element for a card (UI rendering)
 function createCardElement(card) {
     const cardImg = document.createElement("img");
@@ -42,16 +108,8 @@ function createCardElement(card) {
 }
 
 
-homeBtn.addEventListener("click", () => {
-    window.location.href = "index.html";
-});
 
-window.addEventListener("keydown", (event) => {
-    if (event.key === "Esc" || event.key === "Escape") {
-        event.preventDefault();
-        window.location.href = "index.html";
-    }
-});
+
 
 
 async function loadInitialBalance() {
